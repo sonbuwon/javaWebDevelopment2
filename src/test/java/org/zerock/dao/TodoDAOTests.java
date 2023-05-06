9cc1922ee4316cdb70d84b49aff8b504a52f37ff
sonbuwon <bwgh0268@naver.com>
2023-05-06 12:11:31 +0900

    TodoDAO의 등록 기능 구현하기

--- src/test/java/org/zerock/dao/TodoDAOTests.java
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -2,6 +2,7 @@ package org.zerock.dao;
 
 import com.zerock.jdbcex.dao.ConnectionUtil;
 import com.zerock.jdbcex.dao.TodoDAO;
+import com.zerock.jdbcex.domain.TodoVO;
 import lombok.Cleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 
 public class TodoDAOTests {
     private TodoDAO todoDAO;
@@ -22,5 +24,15 @@ public class TodoDAOTests {
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime());
         System.out.println(todoDAO.getTime2());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2023, 05, 06))
+                .build();
+
+        todoDAO.insert(todoVO);
     }
 }
